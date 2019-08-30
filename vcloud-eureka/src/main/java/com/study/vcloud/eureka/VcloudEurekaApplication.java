36186774ca8c34771bb770456363403c26a2d5ce
潘威龙 <weilong.pan@sipingsoft.com>
2019-08-30 17:24:23 +0800

    创建user服务

--- vcloud-eureka/src/main/java/com/study/vcloud/eureka/VcloudEurekaApplication.java
+++ vcloud-eureka/src/main/java/com/study/vcloud/eureka/VcloudEurekaApplication.java
@@ -3,6 +3,9 @@ package com.study.vcloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +13,19 @@ public class VcloudEurekaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VcloudEurekaApplication.class, args);
+    }
+
+    /**
+     * 必须关闭csrf 不然client注册不上
+     * @Date 22:01 2019/7/12
+     **/
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable().httpBasic();
+            super.configure(http);
+        }
     }
 
 }
