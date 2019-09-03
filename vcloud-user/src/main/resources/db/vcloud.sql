f6506cabd9e95ea71d3e593fed8eced94ab78311
潘威龙 <weilong.pan@sipingsoft.com>
2019-09-03 14:24:25 +0800

    修改sql文件

--- vcloud-user/src/main/resources/db/vcloud.sql
+++ vcloud-user/src/main/resources/db/vcloud.sql
@@ -36,7 +36,7 @@ CREATE TABLE `sys_menu`  (
 -- ----------------------------
 -- Records of sys_menu
 -- ----------------------------
-INSERT INTO `sys_menu` VALUES (1, '用户个人信息', '/admin/user/info/**', 'GET', '2018-08-16 04:56:00', '2019-09-03 10:24:29', 1, 1);
+INSERT INTO `sys_menu` VALUES (1, '用户个人信息', '/user/info/**', 'GET', '2018-08-16 04:56:00', '2019-09-03 10:24:29', 1, 1);
 
 -- ----------------------------
 -- Table structure for sys_role
